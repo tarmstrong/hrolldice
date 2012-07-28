3586079771cd3f76f45d1db9e7d7e0bde23db1ff
Tavish Armstrong <tavisharmstrong@gmail.com>
2012-07-28 15:15:46 -0400

    Add Rolldice.Parser module and tests.

---
+++ RollDice/Parser.hs
@@ -0,0 +1,92 @@
+module RollDice.Parser where
+
+import Text.ParserCombinators.Parsec
+
+-- | RollStatement is a command that tells the program how many dice to
+-- | roll, etc. Has Maybes for values that are optional.
+data RollStatement = RollStatement {
+                        rollCount :: Maybe Int
+                      , diceCount :: Maybe Int
+                      , sideCount :: Int
+                      , dropCount :: Maybe Int
+                      , modFuns :: [Int -> Int] }
+
+-- | Take a string and attempt to parse it as a RollStatement.
+parseRollStatement :: String -> Either ParseError RollStatement
+parseRollStatement = parse rollstatement "(unknown)"
+
+-- | parseRollStatement, but wrap the result in Maybe.
+maybeParseRollStatement :: String -> Maybe RollStatement
+maybeParseRollStatement statement =
+  case parseRollStatement statement of
+    Right rollstatement -> Just rollstatement
+    Left  _             -> Nothing
+
+{- Parser for a RollStatement. Examples include:
+ 
+ * d6 : roll a six-sided die.
+ * 4d6 : roll four six-sided die and sum the result.
+ * 2x4d6 : same as 4d6, but perform it twice and show each result separately.
+ * 4d6s1 : same as 4d6 but drop the lowest number rolled.
+ * d6*2+1 : same as d6 but multiply it by 2 and then add 1
+ * d6+1*2 : same as d6 but add 1 and then multiply the result by 2.
+ 
+ In other words, the "modifier functions" at the end are processed in order from left to right. "+1*2-3" can be thought of as "\x -> (((x+1)*2)-3)".
+-}
+rollstatement :: Parser RollStatement
+rollstatement = do
+  nrolls <- optionMaybe . try $ rollcount 
+  ndice <- optionMaybe . try $ dicecount 
+  char 'd'
+  nsides <- many1 digit
+  ndrops <- optionMaybe . try $ dropcount
+  modfuns <- many modfun
+  return RollStatement {
+                      rollCount = nrolls
+                    , diceCount = ndice
+                    , sideCount = read nsides
+                    , dropCount = ndrops
+                    , modFuns = modfuns }
+
+-- | Parser for a roll count.
+-- | "2x1d6" means roll one six-sided die two times
+-- | and show the results separately. This parser
+-- | parses the "2x".
+rollcount :: Parser Int
+rollcount = do
+  count <- many1 digit
+  char 'x'
+  return . read $ count
+
+-- | Parser for dice count.
+-- | "4d6" means roll four six-sided dice. This parser parses
+-- | the "4".
+dicecount :: Parser Int
+dicecount = many1 digit >>= return . read
+
+-- | "s2" means drop the two lowest rolls.
+dropcount :: Parser Int
+dropcount = do
+  char 's'
+  many1 digit >>= return . read
+
+-- | Parser for roll modifiers.
+-- | "d6*2" means roll one six-sided die and double its result.
+-- | "d6-2" means roll one six-sided die and subtract 2 from its result.
+modfun :: Parser (Int -> Int)
+modfun = do
+  op <- oneOf "*+-"
+  operand <- many1 digit
+  let fun = case op of
+        '*' -> (*)
+        '+' -> (+)
+        '-' -> (-)
+  return (fun  (read operand))
+
+
+-- dummyRollStatement = RollStatement {
+--                       rollCount = Just 2
+--                     , diceCount = Just 4
+--                     , sideCount = 6
+--                     , dropCount = Just 1
+--                     , modFuns = [(*2), (+1)] }
--- Test.hs
+++ Test.hs
@@ -1,5 +1,7 @@
 import Test.QuickCheck
-import RollDice.Parser (RollStatement, parseRollStatement)
+import RollDice.Parser (RollStatement, maybeParseRollStatement)
+
+import Data.Maybe (isJust)
 
 goodRollStatements = [
   "d1", "d2", "d6", "d8", "d20", "d100",
@@ -7,11 +9,17 @@ goodRollStatements = [
   "1x4d6", "2x4d6", "6x4d6", "20x20d543", "2xd6",
   "3x4d6s1", "4d6s3",
   "4x4d6s1*2", "4x4d6s1*100",
-  "4x4d6s2*2+3+4-2*4"
-]
+  "4x4d6s2*2+3+4-2*4" ]
 
-successfulParse :: parseResult -> Bool
-successfulParse (Right rollstatement) = True
-successfulParse _                     = False
+badRollStatements = [
+  "10", "2", "0", "2x", "2x3", "+1", "*3", "2x*2", "x", "d",
+  "4x4d-6s1", "2s4x1d6"
+  ]
 
-prop_acceptAllGoodRolls = forAll (elements goodRollStatements) (\s -> (successfulParse . parseRollStatement) s == True)
+prop_acceptAllGoodRolls = forAll (elements goodRollStatements) (\s -> (isJust . maybeParseRollStatement) s == True)
+
+prop_acceptNoBadRolls = forAll (elements badRollStatements) (\s -> (isJust . maybeParseRollStatement) s == False)
+
+main = do
+  quickCheck prop_acceptAllGoodRolls
+  quickCheck prop_acceptNoBadRolls
